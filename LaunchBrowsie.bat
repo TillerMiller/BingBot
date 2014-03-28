cf7fb3537819c08555296cd36e8f74d1983a2185
TillerMiller <tyler.shawn.miller@gmail.com>
2014-03-28 09:46:07 -0700

    Batch script example for use with Windows Task Scheduler

---
+++ LaunchBrowsie.bat
@@ -0,0 +1,4 @@
+cd "C:\Users\tylerm\Documents\RubyProjects\BingBot\"  
+REM This should be the path to where you've downloaded Browsie.  
+"C:\Users\tylerm\Documents\RubyProjects\BingBot\Browsie.rb" 
+REM This simply runs Browsie
